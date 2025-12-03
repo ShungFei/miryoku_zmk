b19a2c612eb27bf68286d49f38ac3ff9060b2c95
Shung Fei <git@shungfei.com>
2025-12-03 18:20:51 +1300

    fill outer keys of eyelash corne

--- miryoku/mapping/42/eyelash_corne.h
+++ miryoku/mapping/42/eyelash_corne.h
@@ -6,15 +6,15 @@
 #define XXX &none
 
 #define MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE( \
-     K00, K01, K02, K03, K04,                     K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                     K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                     K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                     K35, K36, K37, N38, N39 \
+             K00, K01, K02, K03, K04,                                  K05, K06, K07, K08, K09, \
+             K10, K11, K12, K13, K14,                                  K15, K16, K17, K18, K19, \
+             K20, K21, K22, K23, K24,                                  K25, K26, K27, K28, K29, \
+             N30, N31, K32, K33, K34,                                  K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04            XXX       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       XXX  XXX  XXX  K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24  XXX       XXX       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34                      K35  K36  K37
+&u_to_U_BASE K00  K01  K02  K03  K04                   U_MS_U          K05  K06  K07  K08  K09  &u_to_U_BASE \
+&kp LCTRL    K10  K11  K12  K13  K14           U_MS_L  U_BTN1  U_MS_R  K15  K16  K17  K18  K19  XXX \
+&kp LSHFT    K20  K21  K22  K23  K24  &kp C_PP         U_MS_D          K25  K26  K27  K28  K29  XXX \
+                       K32  K33  K34                                   K35  K36  K37
 
 #endif
 
