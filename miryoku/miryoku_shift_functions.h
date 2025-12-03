974fd8a8710f3912f6589ddd50f9e6994e05f3e9
Shung Fei <git@shungfei.com>
2025-12-03 19:01:22 +1300

    disable EP_TOG

--- miryoku/miryoku_shift_functions.h
+++ miryoku/miryoku_shift_functions.h
@@ -9,12 +9,12 @@
   #define U_RGB_HUI &u_rgb_hui
   #define U_RGB_SAI &u_rgb_sai
   #define U_RGB_BRI &u_rgb_bri
-  #define U_EP_TOG &u_ep_tog
+  #define U_EP_TOG &none
 #else
   #define U_RGB_TOG &rgb_ug RGB_TOG
   #define U_RGB_EFF &rgb_ug RGB_EFF
   #define U_RGB_HUI &rgb_ug RGB_HUI
   #define U_RGB_SAI &rgb_ug RGB_SAI
   #define U_RGB_BRI &rgb_ug RGB_BRI
-  #define U_EP_TOG &ext_power EP_TOG
+  #define U_EP_TOG &none
 #endif
